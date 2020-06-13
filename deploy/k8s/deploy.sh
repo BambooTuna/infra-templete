1636360af4aa59019f6037030ff25109ecf2690d
Suzuki Takeo <takeo@stko.info>
2020-06-13 15:28:31 +0900

    add sample app

--- deploy/k8s/deploy.sh
+++ deploy/k8s/deploy.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-kubectl create serviceaccount --namespace kube-system tiller
-kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller
-sleep 20s
+kubectl apply -f load-balancer-example.yaml
