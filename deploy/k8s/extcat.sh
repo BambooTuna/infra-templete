01223f1c165e4c90c56cf152f43637a41dc887f4
Suzuki Takeo <takeo@stko.info>
2020-06-14 10:29:46 +0900

    fix string

--- deploy/k8s/extcat.sh
+++ deploy/k8s/extcat.sh
@@ -7,4 +7,4 @@
 export TPL_LEFT=${TPL_LEFT:-"<%"}
 export TPL_RIGHT=${TPL_RIGHT:-"%>"}
 
-bash <<< "cat <<< "\""$(cat $@ | sed -E "s|\\$|DOLLAR_MARK|g" | sed -E "s|${TPL_LEFT}\ *([^ ]+)\ *${TPL_RIGHT}|$\1|g")"\""" | sed -E "s|DOLLAR_MARK|\\$|g"
+bash <<< "cat <<< "\""$(cat $@ | sed -E "s|\\$|DOLLAR_MARK|g" | sed -E "s|\"|DUBLE_MARK|g" | sed -E "s|${TPL_LEFT}\ *([^ ]+)\ *${TPL_RIGHT}|$\1|g")"\""" | sed -E "s|DOLLAR_MARK|\\$|g" | sed -E "s|DUBLE_MARK|\"|g"
