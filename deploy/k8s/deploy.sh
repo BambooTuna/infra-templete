f77e20838ee8445498dedfcb7bc544470dbca0db
Suzuki Takeo <takeo@stko.info>
2020-06-14 10:10:42 +0900

    add host

--- deploy/k8s/deploy.sh
+++ deploy/k8s/deploy.sh
@@ -4,11 +4,13 @@ if [ $# = 0 ]; then
 	echo "引数(API_SERVER_IMAGE_NAME)なし"
 	exit 1
 else
+  export MYSQL_HOST=mysql-service
+
   export API_SERVER_IMAGE_NAME=$1
 	cat load-balancer-example.tpl.yaml | ./extcat.sh > load-balancer-example.yaml
   kubectl apply -f load-balancer-example.yaml
 
-  helm upgrade --install mysql-service -f ./helm/mysql/mysql-values.yaml stable/mysql
+  helm upgrade --install ${MYSQL_HOST} -f ./helm/mysql/mysql-values.yaml stable/mysql
 fi
 
 
