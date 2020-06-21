83771393460a66fbc35a7ae1e71480a6b9d3266a
Suzuki Takeo <takeo@stko.info>
2020-06-21 20:42:08 +0900

    add grpc code gen

---
+++ tools/grpc/code-gen-ts.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# ./code-gen-ts.sh [proto path] [out file path]
+# ./code-gen-ts.sh ./proto ./pb
+
+IMAGE_NAME='grpc-code-gen:latest'
+
+function build () {
+  mkdir -p $2
+  for file in `\find $1 -name '*.proto'`; do
+      docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}:/${PWD}" -w="/${PWD}" ${IMAGE_NAME} \
+      protoc \
+      -I$1 \
+      -I/usr/local/include/google \
+      --js_out=import_style=commonjs:$2 \
+      --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:$2 \
+      $file
+  done
+}
+
+
+if [ "$(docker image ls -q ${IMAGE_NAME})" ]; then
+  echo "Image ${IMAGE_NAME} already exist."
+else
+  docker build `dirname $0` -t ${IMAGE_NAME}
+fi
+
+build $1 $2
