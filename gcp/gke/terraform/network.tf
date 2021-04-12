d04dd3176133253dec671768d1e832f881f34aa8
BambooTuna <bambootuna@gmail.com>
2021-04-13 01:52:41 +0900

    fix: setup cloudsql proxy

--- gcp/gke/terraform/network.tf
+++ gcp/gke/terraform/network.tf
@@ -11,3 +11,17 @@ resource "google_compute_subnetwork" "subnetwork-01" {
   private_ip_google_access = true
 }
 
+//resource "google_compute_global_address" "private_ip_block" {
+//  name         = "private-ip-block"
+//  purpose      = "VPC_PEERING"
+//  address_type = "INTERNAL"
+//  ip_version   = "IPV4"
+//  prefix_length = 20
+//  network       = google_compute_network.network.self_link
+//}
+//
+//resource "google_service_networking_connection" "private_vpc_connection" {
+//  network                 = google_compute_network.network.self_link
+//  service                 = "servicenetworking.googleapis.com"
+//  reserved_peering_ranges = [google_compute_global_address.private_ip_block.name]
+//}
